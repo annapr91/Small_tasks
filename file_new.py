c763b4a216975436ca6243b485e38c3597fa70e6
Anna Tarassova <annaprusakova91@gmail.com>
2022-09-20 11:06:50 +0300

    modified data

--- file_new.py
+++ file_new.py
@@ -3,7 +3,7 @@ random.seed(13)
 a= [random.randint(1,15) for i in range(25)]
 print(a)
 
-while a!=sorted(a):
+for run in range(len(a)):
      for j in range(len(a)-1):
           if a[j]>a[j+1]:
                a[j],a[j+1]=a[j+1],a[j]
